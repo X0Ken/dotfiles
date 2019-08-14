47b62e79b29a8040a97db18bf7f5161ccfe4984d
wanghongxu <wang19930902@gmail.com>
2019-08-14 13:02:29 +0800

    fix update shell

--- setup.sh
+++ setup.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
 # tmux
-cp ./.tmux.conf ~/
+cp ./tmux.conf ~/.tmux.conf
 
 # vim
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-cp ./vimrc ~/.vim/vimrc
-ln ~/.vimrc ~/.vim/vimrc
+cp ./vimrc ~/.vimrc
 vim +PluginInstall +qall
 
-echo "dont forget to install / update YoucompleteMe"
+echo "dont forget to install / update YoucompleteMe, Leaderf"
 
